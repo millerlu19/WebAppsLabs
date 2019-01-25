e27e8269c21a9541074a64fa5e469b672016c61c
Lucas Miller <millerlu19@hanover.edu>
2019-01-25 14:15:34 -0500

    initialized isOver function

--- Lab3/game.js
+++ Lab3/game.js
@@ -1,8 +1,12 @@
 export default class Game {
 	constructor(maxPoints, points = 0) {
 		this.maxPoints = maxPoints;
-		this.points = points
+		this.points = points;
 	}
 
-
+	isOver() {
+		if (points >= maxPoints) {
+			return true;
+		}
+	}
 }
