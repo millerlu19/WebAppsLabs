c07c743d21eed7e524619f6d6e1070b5a5bd109d
Lucas Miller <millerlu19@hanover.edu>
2019-01-25 14:25:26 -0500

    added values for x, y, radius

--- Lab3/circle.js
+++ Lab3/circle.js
@@ -1,3 +1,8 @@
 export default class Circle {
-
+	constructor(myColor) {
+		this.myColor = myColor;
+		val x = Math.random(0, 600);
+		val y = Math.random(0, 300);
+		val radius = Math.random(10,30);
+	}
 }
