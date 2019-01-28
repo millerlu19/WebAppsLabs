0ee722598963ca553556ff1a303a3332b986bea8
Lucas Miller <millerlu19@hanover.edu>
2019-01-28 14:36:58 -0500

    fixed instance variables in circle class

--- Lab3/circle.js
+++ Lab3/circle.js
@@ -1,8 +1,8 @@
 export default class Circle {
 	constructor(color) {
 		this.color = color;
-		var x = Math.random(0, 600);
-		var y = Math.random(0, 300);
-		var radius = Math.random(10,30);
+		this.x = Math.random()*600;
+		this.y = Math.random()*300;
+		this.radius = (Math.random()*20)+10;
 	}
 }
