61b157a536f7d9a3fbdf42323a46c85519920759
Lucas Miller <millerlu19@hanover.edu>
2019-03-04 14:31:09 -0500

    Beginning to extract score.spec.js file. Ref #2

--- Lab4/test/score.spec.js
+++ Lab4/test/score.spec.js
@@ -3,11 +3,16 @@ import Score from '../js/score.js';
 let expect = chai.expect;
 
 describe('Score instances', () => {
+   let score;
+   beforeEach (() => { score = new Score(); });
    it('start w/ zeros', () => {
-      let score = new Score();
-      expect(score.switchWins).to.equal(0);
-      expect(score.switchLosses).to.equal(0);
-      expect(score.stayWins).to.equal(0);
-      expect(score.stayLosses).to.equal(0);
+      checkValuesAre(0)
    });
+
+   function checkValuesAre(switchWins) {
+   	expect(score.switchWins).to.equal(switchWins);
+    expect(score.switchLosses).to.equal(0);
+    expect(score.stayWins).to.equal(0);
+    expect(score.stayLosses).to.equal(0);
+   }
 });
