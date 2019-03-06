88f0af240e7cc53a66ec77a2b4431bba2fc0c40b
Lucas Miller <millerlu19@hanover.edu>
2019-03-06 13:55:20 -0500

    finished test refactoring. Ref #2

--- Lab4/test/score.spec.js
+++ Lab4/test/score.spec.js
@@ -6,13 +6,13 @@ describe('Score instances', () => {
    let score;
    beforeEach (() => { score = new Score(); });
    it('start w/ zeros', () => {
-      checkValuesAre(0)
+      checkValuesAre(0, 0, 0, 0)
    });
 
-   function checkValuesAre(switchWins) {
+   function checkValuesAre(switchWins, switchLosses, stayWins, stayLosses) {
    	expect(score.switchWins).to.equal(switchWins);
-    expect(score.switchLosses).to.equal(0);
-    expect(score.stayWins).to.equal(0);
-    expect(score.stayLosses).to.equal(0);
+    expect(score.switchLosses).to.equal(switchLosses);
+    expect(score.stayWins).to.equal(stayWins);
+    expect(score.stayLosses).to.equal(stayLosses);
    }
 });
