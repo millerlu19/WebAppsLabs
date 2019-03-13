cfae312cee049b453fd186642b64e31b4946beed
Lucas Miller <millerlu19@hanover.edu>
2019-03-13 14:38:48 -0400

    more implementation with scorecardui class. Ref #3

--- Lab4/js/scorecardui.js
+++ Lab4/js/scorecardui.js
@@ -1,8 +1,50 @@
 // scorecardui.js
 
-export default class ScorecardUI {
+import Observable from './observable.js';
+
+export default class ScorecardUI extends Observable {
 	constructor($el) {
-		this.$el() = $el
+    	super();
+    	this.$el = $el;
+   }
+
+	initialize(o) {
+		this.$el.html(template(o));
 	}
 
+	update(field, newValue) {
+		let $inputEl = this.$el.find("#" + field);
+		$inputEl.val(newValue);
+		highlight($inputEl);
+	}
+
+}
+
+function template(o) {
+	return `
+	<input type="button" id="resetButton" value="Reset"></input>
+	<div class="scoreSection"><h2>SWITCH</h2>
+	   <label for="switchWins" >Wins:
+	      <input type="text" id="switchWins" name="switchWins" value="${o.switchWins}"></input>
+	   </label>
+	   <label for="switchLosses" >Losses:
+	      <input type="text" id="switchLosses" name="switchLosses" value="${o.switchLosses}"></input>
+	   </label>
+	</div>
+	<div class="scoreSection"><h2>STAY</h2>
+	   <label for="stayWins" >Wins:
+	      <input type="text" id="stayWins" name="stayWins" value="${o.stayWins}"></input>
+	   </label>
+	   <label for="switchLosses" >Losses:
+	      <input type="text" id="stayLosses" name="stayLosses" value="${o.stayLosses}"></input>
+	   </label>
+	</div>
+	`;
+}
+
+function highlight(el) {
+	let currentColor = el.css('background-color');
+	let targetColor = "#f47142";
+	el.css('background-color', targetColor);
+	setTimeout(() => el.css('background-color', currentColor), 1000);
 }
--- Lab4/test/scorecarduiTest.js
+++ Lab4/test/scorecarduiTest.js
@@ -2,3 +2,13 @@
 
 import ScorecardUI from '../js/scorecardui.js';
 let scorecard = new ScorecardUI($("#scorecard"));
+
+scorecard.initialize({
+	switchWins: 5, switchLoses: 3,
+	stayWins: 2, stayLosses: 4
+});
+
+scorecard.update("switchLosses", 3 + 1);
+
+scorecard.on("resetRequested", () => console.log("Reset called!"));
+
