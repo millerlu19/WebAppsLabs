f86a1d9d933b294018eb66ae6c2e8551aee3f576
Lucas Miller <millerlu19@hanover.edu>
2019-03-13 14:02:30 -0400

    implementing scorecardui, scorecarduiTest, and testUI. Ref #3

---
+++ Lab4/js/scorecardui.js
@@ -0,0 +1,8 @@
+// scorecardui.js
+
+export default class ScorecardUI {
+	constructor($el) {
+		this.$el() = $el
+	}
+
+}
---
+++ Lab4/test/scorecarduiTest.js
@@ -0,0 +1,4 @@
+// scorecarduiTest.js
+
+import ScorecardUI from '../js/scorecardui.js';
+let scorecard = new ScorecardUI($("#scorecard"));
