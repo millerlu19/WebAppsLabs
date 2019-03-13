018af9f0fa96d2dbd65706ff5700f3d12312adff
Lucas Miller <millerlu19@hanover.edu>
2019-03-13 18:16:20 -0400

    add tests for ScoreCardController class, code in correct place? Ref #4

--- Lab4/test/score.spec.js
+++ Lab4/test/score.spec.js
@@ -3,38 +3,48 @@ import Score from '../js/score.js';
 let expect = chai.expect;
 
 describe('Score instances', () => {
-   let score;
-   beforeEach (() => { score = new Score(); });
-   it('start w/ zeros', () => {
-      checkValuesAre(0, 0, 0, 0);
-   });
-   it('score class increments appropriate value', () => {
-   	score.addResult(Score.ACTION_SWITCH, Score.RESULT_WIN);
-  	checkValuesAre(1, 0, 0, 0);
-  	score.addResult(Score.ACTION_SWITCH, Score.RESULT_LOSS);
-  	checkValuesAre(1, 1, 0, 0);
-  	score.addResult(Score.ACTION_STAY, Score.RESULT_WIN);
-  	checkValuesAre(1, 1, 1, 0);
-  	score.addResult(Score.ACTION_STAY, Score.RESULT_LOSS);
-  	checkValuesAre(1, 1, 1, 1);
-   });
-   it('set all counters to zeros', () => {
-   	score.addResult(Score.ACTION_SWITCH, Score.RESULT_WIN);
-  	checkValuesAre(1, 0, 0, 0);
-  	score.addResult(Score.ACTION_SWITCH, Score.RESULT_LOSS);
-  	checkValuesAre(1, 1, 0, 0);
-  	score.addResult(Score.ACTION_STAY, Score.RESULT_WIN);
-  	checkValuesAre(1, 1, 1, 0);
-  	score.addResult(Score.ACTION_STAY, Score.RESULT_LOSS);
-  	checkValuesAre(1, 1, 1, 1);
-  	score.reset();
-  	checkValuesAre(0, 0, 0, 0);
-   });
+	let score;
+	beforeEach (() => { score = new Score(); });
+	it('start w/ zeros', () => {
+    	checkValuesAre(0, 0, 0, 0);
+	});
+	it('score class increments appropriate value', () => {
+   		score.addResult(Score.ACTION_SWITCH, Score.RESULT_WIN);
+  		checkValuesAre(1, 0, 0, 0);
+  		score.addResult(Score.ACTION_SWITCH, Score.RESULT_LOSS);
+  		checkValuesAre(1, 1, 0, 0);
+  		score.addResult(Score.ACTION_STAY, Score.RESULT_WIN);
+  		checkValuesAre(1, 1, 1, 0);
+  		score.addResult(Score.ACTION_STAY, Score.RESULT_LOSS);
+  		checkValuesAre(1, 1, 1, 1);
+	});
+	it('set all counters to zeros', () => {
+   		score.addResult(Score.ACTION_SWITCH, Score.RESULT_WIN);
+  		checkValuesAre(1, 0, 0, 0);
+  		score.addResult(Score.ACTION_SWITCH, Score.RESULT_LOSS);
+  		checkValuesAre(1, 1, 0, 0);
+  		score.addResult(Score.ACTION_STAY, Score.RESULT_WIN);
+  		checkValuesAre(1, 1, 1, 0);
+  		score.addResult(Score.ACTION_STAY, Score.RESULT_LOSS);
+  		checkValuesAre(1, 1, 1, 1);
+  		score.reset();
+  		checkValuesAre(0, 0, 0, 0);
+	});
 
-   function checkValuesAre(switchWins, switchLosses, stayWins, stayLosses) {
-   	expect(score.switchWins).to.equal(switchWins);
-    expect(score.switchLosses).to.equal(switchLosses);
-    expect(score.stayWins).to.equal(stayWins);
-    expect(score.stayLosses).to.equal(stayLosses);
-   }
+	function checkValuesAre(switchWins, switchLosses, stayWins, stayLosses) {
+   		expect(score.switchWins).to.equal(switchWins);
+    	expect(score.switchLosses).to.equal(switchLosses);
+    	expect(score.stayWins).to.equal(stayWins);
+    	expect(score.stayLosses).to.equal(stayLosses);
+	}
+
+	it('trigger messages when values change (switchWins)', (done) => {
+		let h = (msg, value) => {
+	    	expect(msg).to.equal('switchWins');
+	    	expect(value).to.equal(2);
+	    	done();
+		};
+		score.on('change', h);
+		score.trigger('change', 'switchWins', 1);
+	});
 });
